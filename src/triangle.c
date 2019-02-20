7f00dd74359ac0c18b441f74af71fb9a4c872d53
Philippe Loctaux <p@philippeloctaux.com>
2019-02-20 18:48:06 +0100

    updated draw_triangle function, doesn't compile

--- src/triangle.c
+++ src/triangle.c
@@ -30,15 +30,31 @@ void    sort_pos_triangle(sfVector2f *pos)
 
 void    draw_triangle(my_framebuff_t *buff, sfVector2f *pos, sfColor color)
 {
+    sfVector2f A = pos[0];
+    sfVector2f B = pos[1];
+    sfVector2f C = pos[2];
+    sfVector2f E = A;
+    sfVector2f S = A;
+    double dx1;
+    double dx2;
+    double dx3;
+
     sort_pos_triangle(pos);
-    double dx1 = (pos[1].y - pos[0].y > 0) ?
-(pos[1].x - pos[0].x) / (pos[1].y - pos[0].y) : 0;
-    double dx2 = (pos[2].y - pos[0].y > 0) ?
-(pos[2].x - pos[0].x) / (pos[2].y - pos[0].y) : 0;
-    double dx3 = (pos[2].y - pos[0].y > 0) ?
-(pos[1].x - pos[0].x) / (pos[1].y - pos[0].y) : 0;
-    sfVector2f S = pos[0];
-    sfVector2f E = pos[0];
+    dx1 = ((B.y - A.y) > 0) ? (B.x - A.x) / (B.y - A.y) : 0;
+    dx2 = ((C.y - A.y) > 0) ? (C.x - A.x) / (C.y - A.y) : 0;
+    dx3 = ((C.y - B.y) > 0) ? (C.x - B.x) / (C.y - B.y) : 0;
 
-
+    if (dx1 > dx2) {
+        for (; S.y <= B.y; S.y++, E.y++, S.x+=dx2, E.x+=dx1)
+            draw_line(buff, S.x,E.x,S.y, color);
+        E = B;
+        for(; S.y<=C.y; S.y++, E.y++, S.x+=dx2, E.x+=dx3)
+            draw_line(buff, S.x,E.x,S.y, color);
+    } else {
+        for(; S.y<=B.y; S.y++, E.y++, S.x+=dx1, E.x+=dx2)
+            draw_line(buff, S.x,E.x,S.y, color);
+        S = B;
+        for(; S.y<=C.y; S.y++, E.y++, S.x+=dx3, E.x+=dx2)
+            draw_line(buff, S.x,E.x,S.y, color);
+    }
 }
