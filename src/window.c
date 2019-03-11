70ec80166b6f5613bba7ad0513a0469d3f3f3a29
Philippe Loctaux <p@philippeloctaux.com>
2019-03-11 11:30:06 +0100

    window: fixed coding style issue

--- src/window.c
+++ src/window.c
@@ -10,6 +10,24 @@
 #include "world.h"
 #include "my.h"
 
+void increment_variables(my_game_t *game) 
+{
+    game->map->roll += game->map->roll_fg;
+    game->map->yaw += game->map->yaw_fg;
+    game->map->pitch += game->map->pitch_fg;
+    (game->map->roll >= 360) ? game->map->roll -= 360 : 0;
+    (game->map->roll < 0) ? game->map->roll += 360 : 0;
+    (game->map->yaw >= 360) ? game->map->yaw -= 360 : 0;
+    (game->map->yaw < 0) ? game->map->yaw += 360 : 0;
+    (game->map->pitch >= 360) ? game->map->pitch -= 360 : 0;
+    (game->map->pitch < 0) ? game->map->pitch += 360 : 0;
+    game->map->roll_fg = 0;
+    game->map->yaw_fg = 0;
+    game->map->pitch_fg = 0;
+    game->map->move_point_x = 0;
+    game->map->move_point_y = 0;
+}
+
 int     window(char *str)
 {
     my_game_t    *game = NULL;
@@ -19,33 +37,12 @@ int     window(char *str)
     while (sfRenderWindow_isOpen(game->win->window)) {
         if (game->map->update == 1) {
             game->map->update = 0;
-
             clear_z_buff(game->win->z_buff);
             clear_t_buff(game->win->t_buff);
             clear_buff(game->win->framebuff);
-
             transform_move(game);
             rotation(game->map);
-            game->map->roll += game->map->roll_fg;
-            game->map->yaw += game->map->yaw_fg;
-            game->map->pitch += game->map->pitch_fg;
-            if (game->map->roll >= 360)
-                game->map->roll -= 360;
-            if (game->map->roll < 0)
-                game->map->roll += 360;
-            if (game->map->yaw >= 360)
-                game->map->yaw -= 360;
-            if (game->map->yaw < 0)
-                game->map->yaw += 360;
-            if (game->map->pitch >= 360)
-                game->map->pitch -= 360;
-            if (game->map->pitch < 0)
-                game->map->pitch += 360;
-            game->map->roll_fg = 0;
-            game->map->yaw_fg = 0;
-            game->map->pitch_fg = 0;
-            game->map->move_point_x = 0;
-            game->map->move_point_y = 0;
+            increment_variables(game);
             to_2d(game);
             display(game);
         }
