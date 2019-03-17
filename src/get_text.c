9ffe1044b14e75f2c679f2d289417514571962e8
Nicolas Duchesne <nicolasduchesne884@gmail.com>
2019-03-17 19:49:38 +0100

    fix -h

--- src/get_text.c
+++ src/get_text.c
@@ -10,16 +10,16 @@
 const   int letter[40] = {sfKeyA, sfKeyB, sfKeyC, sfKeyD, sfKeyE, sfKeyF,
 sfKeyG, sfKeyH, sfKeyI, sfKeyJ, sfKeyK, sfKeyL, sfKeyM, sfKeyN, sfKeyO, sfKeyP,
 sfKeyQ, sfKeyR, sfKeyS, sfKeyT, sfKeyU, sfKeyV, sfKeyW, sfKeyX, sfKeyY, sfKeyZ,
-sfKeySlash, sfKeyPeriod, sfKeyHyphen, sfKeyNum0, sfKeyNum1, sfKeyNum2,
+sfKeySlash, sfKeyPeriod, sfKeyNum0, sfKeyNum1, sfKeyNum2,
 sfKeyNum3, sfKeyNum4, sfKeyNum5, sfKeyNum6, sfKeyNum7, sfKeyNum8, sfKeyNum9,
 sfKeyTilde};
-const char *list_letters = "abcdefghijklmnopqrstuvwxyz/.-0123456789~";
+const char *list_letters = "abcdefghijklmnopqrstuvwxyz/.0123456789~";
 
 void    write_on_txt(my_game_t *game)
 {
     int i = 0;
 
-    while (i < 40) {
+    while (i < 39) {
         if (sfKeyboard_isKeyPressed(letter[i]) &&
 (game->last_input != list_letters[i])) {
             game->text[game->text_nb] = list_letters[i];
--- src/main.c
+++ src/main.c
@@ -11,17 +11,20 @@
 int     display_usage(const char *av)
 {
     my_printf("USAGE\n %s\t[map]\n map\t\tobject file to load in game\n\n", av);
-    my_printf("USER INTERACTION\nbuttons to zoom, move, rotate the object\n");
-    my_printf("load file, save current object, change texture/color\n\n");
+    my_printf("USER INTERACTION\n buttons to zoom\t\t'q'/'e'\n buttons to size");
+    my_printf(" up\t'z'/'x'\n move\t\t\t'arrow'/'up key'/'down key'");
+    my_printf("\n\nrotate the object:\n ROLL\t'j'/'u'\n YAW\t'k'/'i'\n");
+    my_printf(" PITCH\t'l'/'o'\n\n");
+    my_printf("load file, save current object, change texture/color\n");
     my_printf("filename must be composed of letters and numbers\n");
-    return 0;
+    return (0);
 }
 
 int     main(int ac, char **av)
 {
     if (ac > 2)
         return (84);
-    if (ac > 1 && ((!my_strcmp(av[1], "-h")) || (!my_strcmp(av[1], "--help"))))
+    if (ac > 1 && (!my_strcmp(av[1], "-h") || !my_strcmp(av[1], "--help")))
         return (display_usage(av[0]));
     return (window(av[1]));
 }
